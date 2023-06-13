5e824712d02105a2bb3982248ab541b5fd66f74d
Gab Richard <gabrichard@MBP-de-Gab.students.ver1fr.ecole-hexagone.com>
2023-06-13 17:40:24 +0200

    begin project

---
+++ Swipe Time/Swipe_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swipe_TimeApp.swift
+//  Swipe Time
+//
+//  Created by Gab Richard on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Swipe_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
